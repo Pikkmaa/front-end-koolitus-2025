2a62d5c69d41a886a02e2ff8074eac3fe4beac86
Toomas Pikkmaa <60581507+Pikkmaa@users.noreply.github.com>
2025-07-15 22:19:17 +0300

    Portfoolio edasiarendus

--- portfoolio/src/App.jsx
+++ portfoolio/src/App.jsx
@@ -9,22 +9,26 @@ function App() {
   return (
     <>
       <div>
-        <img className='peatsi-pilt' src="https://cdn.midjourney.com/8e3ba7f3-f306-4f7f-bc09-945cbe075aee/0_1.png" alt="Muses" />
-        <div className='joon'></div>
+        <img className="peatsi-pilt" src="https://cdn.midjourney.com/8e3ba7f3-f306-4f7f-bc09-945cbe075aee/0_1.png" alt="Muses" />
+        <div className="joon"></div>
 
      
 
-      <div className='navigatsiooni-pilt'>
+      <div className="navigatsiooni-pilt">
         <Link className= "main-link" to="work">
         <img src="https://cdn.midjourney.com/da8d8899-f519-4492-a890-433b12d9817f/0_1.png" alt="" />
+        <div>Varasem töökogemus</div>
         </Link>
         <Link className= "main-link" to="hobbies">
         <img src="https://cdn.midjourney.com/f96e9d36-54d9-474b-b45b-bc65531e4e09/0_0.png" alt="" />
+        <div>Minu harrastused</div>
         </Link>
         <Link className= "main-link" to="courses">
         <img src="https://cdn.midjourney.com/fca61009-6669-495a-b677-4d178ac34f30/0_0.png" alt="" />
+        <div>Minu koolitee</div>
         </Link>
         </div>
+        <br />
 
         <Routes>
           <Route path='work' element={ < Work /> } />
@@ -32,10 +36,10 @@ function App() {
           <Route path='courses' element={ < Courses /> } />
         </Routes>
 
-
-
-
-
+        <a className="facebook-nupp" href="https://www.facebook.com/NightFliesHard">
+        <img src="/facebook.png" alt="" />
+        <span>Facebook</span>
+        </a>
       </div>
     </>
   )
--- portfoolio/src/pages/Hobbies.jsx
+++ portfoolio/src/pages/Hobbies.jsx
@@ -5,7 +5,19 @@ function Hobbies() {
         <Link to="/">
         <button>Tagasi</button>
         </Link>
-        <div>Hobbies</div>
+        <div>Harrastused</div>
+
+        <div><strong>Aktiivne muusik</strong></div>
+        <iframe
+      width="560" 
+      height="315" 
+      src="https://www.youtube.com/embed/iZNgAKUD4PY?si=Bh4xvXsxXpucds9a" 
+      title="YouTube video player" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+      referrerpolicy="strict-origin-when-cross-origin" 
+      allowfullscreen>
+      </iframe>
     </div>
   )
 }
