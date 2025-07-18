57740561ed4f71ce1e63d30ef395f45a2a203417
Toomas Pikkmaa <60581507+Pikkmaa@users.noreply.github.com>
2025-07-18 20:43:05 +0300

    tegelased

---
+++ tegelased/src/pages/LisaTegelane.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+
+function LisaTegelane() {
+    const [sonum, setSonum ] = useState("");
+  return (
+    <div>
+        <div>{sonum}</div>
+        <label>Tegelase nimi</label> <br />
+        <input type="text" /> <br />
+        <button onClick={() => setSonum("Tegelane lisatud!")} >Lisa uus</button> <br />
+    </div>
+  )
+}
+
+export default LisaTegelane
