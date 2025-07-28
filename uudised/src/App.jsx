29a163a3bba6d934f690a8387de4416fe05e0886
Toomas Pikkmaa <60581507+Pikkmaa@users.noreply.github.com>
2025-07-28 11:55:01 +0300

    uudised login btn

--- uudised/src/App.jsx
+++ uudised/src/App.jsx
@@ -5,12 +5,39 @@ import Kontakt from './pages/Kontakt'
 import Meist from './pages/Meist'
 import Uudised from './pages/Uudised'
 import Seaded from './pages/Seaded'
+import { useState, useRef } from 'react'
 
 function App() {
+  const [sisselogitud, setSisselogitud] = useState("ei");
+  const [sonum, setSonum] = useState("");
+  const kasutjaNimiRef = useRef("");
+  const paroolRef = useRef("");
 
+  function logiSisse() {
+    if (paroolRef.current.value === "123") {
+      setSisselogitud("jah");
+      setSonum(kasutjaNimiRef.current.value +", oled sisslogitud!");
+      } else {
+        setSonum("Vale parool!");
+    }
+    
+  }
   return (
     <>
       <div>
+        <div>{sonum}</div>
+        {sisselogitud === "ei" && <div>
+          <label>kasutajanimi</label> <br />
+          <input ref={kasutjaNimiRef} type="text" /> <br />
+          <label>parool</label> <br />
+          <input ref={paroolRef} type="text" />
+        </div>}
+
+
+        {sisselogitud === "ei" && <button onClick={logiSisse} >Logi sisse</button>}
+        {sisselogitud === "jah" && <button onClick={() => setSisselogitud("ei")} >Logi välja</button>}
+
+        <br />
         <Link to='/'>
         <button>Avalehele</button>
         </Link>
