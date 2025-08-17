55041a9f8b29abc8efebb45e1475c2592e22c050
Toomas Pikkmaa <60581507+Pikkmaa@users.noreply.github.com>
2025-08-17 21:18:25 +0300

    tagasiside

--- tagasiside/src/pages/Kasutajad.jsx
+++ tagasiside/src/pages/Kasutajad.jsx
@@ -39,6 +39,31 @@ function Kasutajad() {
     kasutajadFailist.splice(index,1);
     setKasutajad(kasutajadFailist.slice());
   }
+
+  function lisaTelefonAlgus() {
+  const uued = kasutajad.map(kasutaja => ({
+    ...kasutaja,
+    phone: "000-" + kasutaja.phone
+  }));
+  setKasutajad(uued);
+}
+
+  function ainultEmailid() {
+    const emailid = kasutajad.map(kasutaja => kasutaja.email);
+    console.log("Emailide massiiv:", emailid);
+  }
+
+  function asendaCatchPhrase() {
+  const uued = kasutajad.map(kasutaja => ({
+    ...kasutaja,
+    company: {
+      ...kasutaja.company,
+      catchPhrase: kasutaja.company.catchPhrase.replace(/a/g, "e")
+    }
+  }));
+  setKasutajad(uued);
+}
+
   return (
     <div>
         <div>Kuvatud on {kasutajad.length} kasutajat.</div>
@@ -47,12 +72,25 @@ function Kasutajad() {
         <button onClick={kuvaEsimeneC}>Kuva esimene kasutaja, kelle nimie esimene täht on C</button>
         <button onClick={sorteeriLatJargi}>Sorteeri kasutajad "lat" väärtuse järgi</button>
         <button onClick={liidaId}>Liida kokku kõkide kasutajate id-d consloe log'i</button>
+        <button onClick={lisaTelefonAlgus}>Lisa 000- telefoni numbri ette</button>
+        <button onClick={ainultEmailid}>ainult emailid console log'is</button>
+        <button onClick={asendaCatchPhrase}>asenda tähed CatchPhrase'is</button>
         <div>
         {kasutajad.map((kasutaja, index) => <div key={kasutaja.id}>
             <div>{kasutaja.id}</div>
             <div>{kasutaja.name}</div>
             <div>{kasutaja.username}</div>
+            <div>{kasutaja.email}</div>
             <div>{kasutaja.address.geo.lat}</div>
+            <div>{kasutaja.address.geo.ing}</div>
+            <div>{kasutaja.address.street}</div>
+            <div>{kasutaja.address.suite}</div>
+            <div>{kasutaja.address.city}</div>
+            <div>{kasutaja.address.zipcode}</div>
+            <div>{kasutaja.phone}</div>
+            <div>{kasutaja.company.name}</div>
+            <div>{kasutaja.company.catchPhrase}</div>
+            <div>{kasutaja.company.bs}</div>
             <button onClick={()=>kustuta(index)}>X</button>
             </div> )}
     </div>
